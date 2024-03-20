5162753c4c7a130feef1c64a20cbfca3937e4b6f
Caesar <ignatiuscaesar16@gmail.com>
2024-03-21 00:52:00 +0300

    Edits the js file

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code in this file!
+// Define the current user
+const currentUser = 'Grace Hopper';
+
+// Create a welcome message using string interpolation
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+// Generate an excited welcome message by converting the above message to uppercase
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+// Formulate a short greeting that includes only the first initial of the currentUser's name
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
